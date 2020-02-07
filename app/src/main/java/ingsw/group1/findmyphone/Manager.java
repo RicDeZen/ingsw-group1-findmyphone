b5e5b36b354a574adea5a14eeedccfc6b37c0345
Giorgia Bortoletti <56470846+bortoletti-giorgia@users.noreply.github.com>
2020-02-07 20:09:40 +0100

    Resolved some conflicts with branch development

--- app/src/main/java/ingsw/group1/findmyphone/Manager.java
+++ app/src/main/java/ingsw/group1/findmyphone/Manager.java
@@ -4,53 +4,70 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import ingsw.group1.findmyphone.activity.ActivityConstantsUtils;
+import ingsw.group1.findmyphone.alarm.AlarmManager;
+import ingsw.group1.findmyphone.location.CommandResponseLocation;
+import ingsw.group1.findmyphone.location.LocationManager;
 import ingsw.group1.msglibrary.ReceivedMessageListener;
 import ingsw.group1.msglibrary.SMSManager;
 import ingsw.group1.msglibrary.SMSMessage;
 import ingsw.group1.msglibrary.SMSPeer;
 
-/***
+/**
+ * This is the main manager
+ *
  * @author Pardeep
- * Class
+ * @author Giorgia Bortoletti (refactoring)
  */
-
 public class Manager {
-    private static Context currentContext;
-    AlarmManager alarmManager = new AlarmManager();
-    LocationManager locationManager=new LocationManager();
-    SMSManager smsManager;
-    Constants constants=new Constants();
-
     private static final String MANAGER_TAG = "Manager";
-    private  SendResponseSms sendResponseSms;
+    private static Context currentContext;
 
-    public Manager(Context context){
-        currentContext=context.getApplicationContext();
-        smsManager=SMSManager.getInstance(Manager.currentContext);
+    private AlarmManager alarmManager;
+    private LocationManager locationManager;
+    private SMSManager smsManager;
+    private CommandResponseLocation sendResponseSms;
 
+    /**
+     * Constructor
+     *
+     * @param context application context
+     */
+    public Manager(Context context) {
+        currentContext = context.getApplicationContext();
+        locationManager = new LocationManager();
+        alarmManager = new AlarmManager();
+        smsManager = SMSManager.getInstance(Manager.currentContext);
     }
+
+    //---------------------------- LISTENERS ----------------------------
 
     /**
      * Method to remove receivedListener
      */
-    public void removeReceiveListener() {smsManager.removeReceiveListener(); }
+    public void removeReceiveListener() {
+        smsManager.removeReceiveListener();
+    }
 
     /**
      * Setter for receivedListener in SMSHandler
      *
      * @param newReceivedListener the new listener
      */
-    public void setReceiveListener(ReceivedMessageListener<SMSMessage> newReceivedListener) { smsManager.setReceiveListener(newReceivedListener); }
+    public void setReceiveListener(ReceivedMessageListener<SMSMessage> newReceivedListener) {
+        smsManager.setReceiveListener(newReceivedListener);
+    }
+
+    //---------------------------- SEND REQUEST ----------------------------
 
     /**
      * @author Turcato
-     *Send an urgent message to the peer with a location request
+     * Send an urgent message to the peer with a location request
      *
      * @param smsPeer the peer to which  send sms Location request
      */
-    public void sendLocationRequest(SMSPeer smsPeer)
-    {
-        String requestStringMessage = LocationManager.locationMessages[LocationManager.request];
+    public void sendLocationRequest(SMSPeer smsPeer) {
+        String requestStringMessage = locationManager.getRequestLocationMessage();
         SMSMessage smsMessage = new SMSMessage(smsPeer, requestStringMessage);
         smsManager.sendUrgentMessage(smsMessage);
     }
@@ -61,76 +78,59 @@ public class Manager {
      *
      * @param smsPeer the peer to which  send sms Location & alarm request
      */
-    public void sendAlarmRequest(SMSPeer smsPeer)
-    {
-        String requestStringMessage = AlarmManager.audioAlarmMessages[LocationManager.request];
+    public void sendAlarmRequest(SMSPeer smsPeer) {
+        String requestStringMessage = alarmManager.getAlarmRequestMessage();
         SMSMessage smsMessage = new SMSMessage(smsPeer, requestStringMessage);
         smsManager.sendUrgentMessage(smsMessage);
     }
+
+    //---------------------------- ACTIONS AFTER RECEIVING A REQUEST ----------------------------
 
     /**
-     * @author Turcato
-     * Send an urgent message to the peer for an Alarm and Location request
-     *
-     * @param smsPeer the peer to which send sms alarm request
-     */
-    public void sendAlarmAndLocationRequest(SMSPeer smsPeer)
-    {
-        //Wake key to indicate urgency to the device
-        String requestStringMessage = AlarmManager.audioAlarmMessages[LocationManager.request]
-                +LocationManager.locationMessages[LocationManager.request];
-        SMSMessage smsMessage = new SMSMessage(smsPeer, requestStringMessage);
-        smsManager.sendUrgentMessage(smsMessage);
-    }
-
-    /***
-     * This method check the received string and can active alarm /send location or both based on its content
+     * This method checks the received string and can active alarm or send location based on its content
      *
      * @param requestMessage the request message that decide which action to do
-     * @param phoneNumber the number to which send you'r phone's location
+     * @param phoneNumber    the number to which send your phone's location or active alarm
      */
-    public void getRequest(String requestMessage,String phoneNumber)
-    {
-        if (locationManager.containsLocationRequest(requestMessage)) {
+    public void analyzeRequest(String requestMessage, String phoneNumber) {
+        if (locationManager.isLocationRequest(requestMessage)) {
             //Action to execute when device receives a Location request
-            sendResponseSms = new SendResponseSms(phoneNumber, currentContext.getApplicationContext());
+            sendResponseSms = new CommandResponseLocation(phoneNumber, currentContext.getApplicationContext());
             locationManager.getLastLocation(currentContext.getApplicationContext(), sendResponseSms);
         }
         //User has to close app manually to stop the alarm
-        if (alarmManager.containsAlarmRequest(requestMessage))
+        if (alarmManager.isAlarmRequest(requestMessage))
             alarmManager.startAlarm(currentContext.getApplicationContext());
     }
 
-    /***
-     *Based on the response this method opens the activityClass or open the default map app
+    /**
+     * Based on the response this method opens the activityClass or open the default map app
+     *
      * @param messageResponse The message received
      */
-   public void getResponse(SMSMessage messageResponse,Class activityClass)
-    {
-        String requestMessage  = messageResponse.getData();
-        if (locationManager.containsLocationRequest(requestMessage)
-                || alarmManager.containsAlarmRequest(requestMessage)) {
-            OpenRequestsActivity(requestMessage, messageResponse.getPeer().getAddress(),activityClass);
+    public void activeResponse(SMSMessage messageResponse, Class activityClass) {
+        String requestMessage = messageResponse.getData();
+        if (locationManager.isLocationRequest(requestMessage)
+                || alarmManager.isAlarmRequest(requestMessage)) {
+            openRequestsActivity(requestMessage, messageResponse.getPeer().getAddress(), activityClass);
         }
 
         //The only expected response
-        if(locationManager.containsLocationResponse(requestMessage)){
+        if (locationManager.isLocationResponse(requestMessage)) {
             Double longitude;
             Double latitude;
             try {
-                longitude = Double.parseDouble(locationManager.getLongitude(requestMessage));
-                latitude = Double.parseDouble(locationManager.getLatitude(requestMessage));
-                locationManager.OpenMapsUrl(currentContext, latitude, longitude);
-            }
-            catch (Exception e){
+                longitude = Double.parseDouble(locationManager.getLongitudeFrom(requestMessage));
+                latitude = Double.parseDouble(locationManager.getLatitudeFrom(requestMessage));
+                locationManager.openMapsUrl(currentContext, latitude, longitude);
+            } catch (Exception e) {
                 //Written in log for future users to report
-                Log.e(MANAGER_TAG,AlarmManager.response + ": " + e.getMessage());
+                Log.e(MANAGER_TAG, e.getMessage());
             }
-
         }
     }
 
-    /***
+    /**
      * @author Turcato
      * Opens an activityClass, forwarding the receivedMessageText and the receivedMessageReturnAddress
      *
@@ -138,15 +138,13 @@ public class Manager {
      * @param receivedMessageText the text of the request message
      * @param receivedMessageReturnAddress the return address of the request message
      */
-    private void OpenRequestsActivity(String receivedMessageText, String receivedMessageReturnAddress, Class activityClass)
-    {
+    private void openRequestsActivity(String receivedMessageText, String receivedMessageReturnAddress, Class activityClass) {
         Log.d(MANAGER_TAG, "OpenRequestsActivity");
         Intent openAlarmAndLocateActivityIntent = new Intent(currentContext.getApplicationContext(), activityClass);
-        openAlarmAndLocateActivityIntent.putExtra(constants.receivedStringMessage, receivedMessageText);
-        openAlarmAndLocateActivityIntent.putExtra(constants.receivedStringAddress, receivedMessageReturnAddress);
+        openAlarmAndLocateActivityIntent.putExtra(ActivityConstantsUtils.RECEIVED_STRING_MESSAGE, receivedMessageText);
+        openAlarmAndLocateActivityIntent.putExtra(ActivityConstantsUtils.RECEIVED_STRING_ADDRESS, receivedMessageReturnAddress);
         openAlarmAndLocateActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         currentContext.getApplicationContext().startActivity(openAlarmAndLocateActivityIntent);
     }
-
 
 }
--- app/src/main/java/ingsw/group1/findmyphone/activity/MainActivity.java
+++ app/src/main/java/ingsw/group1/findmyphone/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package ingsw.group1.findmyphone;
+package ingsw.group1.findmyphone.activity;
 
 import android.Manifest;
 import android.content.Intent;
@@ -7,19 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import ingsw.group1.findmyphone.activity.CreateContactActivity;
+import ingsw.group1.findmyphone.Manager;
+import ingsw.group1.findmyphone.R;
 import ingsw.group1.msglibrary.ReceivedMessageListener;
+import ingsw.group1.msglibrary.SMSManager;
 import ingsw.group1.msglibrary.SMSMessage;
 import ingsw.group1.msglibrary.SMSPeer;
 
 /**
  * @author Turcato, Kumar
  */
-
 public class MainActivity extends AppCompatActivity implements ReceivedMessageListener<SMSMessage> {
 
     private static final String[] permissions = {
@@ -32,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
             Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE
     };
-
     private final int APP_PERMISSION_REQUEST_CODE = 1;
 
     private EditText txtPhoneNumber;
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
     private Manager manager;
     private SMSPeer smsPeer;
 
-    /**
+    /***
      * @author Turcato
      * @param savedInstanceState system parameter
      */
@@ -53,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
         setContentView(R.layout.activity_main);
 
         Button viewContacts;
-      
+
         txtPhoneNumber = findViewById(R.id.phoneNumber);
         sendButton = findViewById(R.id.sendButton);
         sendAlarmRequestButton = findViewById(R.id.sendAlarmRequestButton);
@@ -62,15 +63,6 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
 
         manager = new Manager(getApplicationContext());
         manager.setReceiveListener(this);
-
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                smsPeer = new SMSPeer(txtPhoneNumber.getText().toString());
-                manager.sendAlarmAndLocationRequest(smsPeer);
-            }
-        });
 
         sendLocationRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,6 +86,9 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
                 startActivity(new Intent(MainActivity.this, CreateContactActivity.class));
             }
         });
+
+        //TODO not the optimal place for this, should be moved elsewhere.
+        SMSManager.getInstance(getApplicationContext()).setActivityToWake(AlarmAndLocateResponseActivity.class);
     }
 
 
@@ -101,37 +96,42 @@ public class MainActivity extends AppCompatActivity implements ReceivedMessageLi
     protected void onStart() {
         requestPermissions();
         super.onStart();
+
     }
 
-    /**
+    /***
      * @author Turcato
      * Requests Android permissions if not granted
      */
     public void requestPermissions() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)+
-                ContextCompat.checkSelfPermission(this, Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) +
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) +
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE)
                 != PackageManager.PERMISSION_GRANTED)
 
-        ActivityCompat.requestPermissions(this, permissions, APP_PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, permissions, APP_PERMISSION_REQUEST_CODE);
     }
 
     /***
      * @author Turcato
      * This method is executed both when the app is running or not.
-     * Based on the message's content, opens AlarmAndLocateResponseActivity if it's a request message,
-     * otherwise if it contains the location response (the only one expected) it opens the default maps application
+     * Based on the message's content, opens AlarmAndLocateResponseActivity if it's a request
+     * message,
+     * otherwise if it contains the location response (the only one expected) it opens the
+     * default maps application
      * to the received location
      *
      * @param message Received SMSMessage class of SmsHandler library
      */
     public void onMessageReceived(SMSMessage message) {
-        manager.getResponse(message, AlarmAndLocateResponseActivity.class);
+        manager.activeResponse(message, AlarmAndLocateResponseActivity.class);
     }
 
     /**
