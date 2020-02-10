7e0ff403d2d63282e29cc4291ea88fd9704d941d
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-02-10 16:06:21 +0100

    rmv: Removed everything unrelated to events to allow better modularity of PR

--- app/src/main/java/ingsw/group1/findmyphone/event/LoggableEventDatabase.java
+++
@@ -1,9 +0,0 @@
-package ingsw.group1.findmyphone.event;
-
-/**
- * Interface defining an {@link EventDatabase} containing {@link LoggableEvent} Objects.
- *
- * @param <L>
- */
-public interface LoggableEventDatabase<L extends LoggableEvent> extends EventDatabase<L> {
-}
