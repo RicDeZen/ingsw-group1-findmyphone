a771bdcc95d97902d6bbd3d0af5b647f55748142
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-03-07 16:23:43 +0100

    feat: added animation for stop ring button

--- app/src/main/java/ingsw/group1/findmyphone/activity/AlarmActivity.java
+++ app/src/main/java/ingsw/group1/findmyphone/activity/AlarmActivity.java
@@ -3,12 +3,17 @@ package ingsw.group1.findmyphone.activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
 import ingsw.group1.findmyphone.R;
 import ingsw.group1.findmyphone.RingService;
@@ -17,6 +22,8 @@ import ingsw.group1.findmyphone.RingService;
  * @author Riccardo De Zen.
  */
 public class AlarmActivity extends AppCompatActivity {
+
+    private ImageButton stopRingButton;
 
     /**
      * Anonymous class defining the connection to the service.
@@ -60,12 +67,45 @@ public class AlarmActivity extends AppCompatActivity {
         win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         setContentView(R.layout.alarm_activity);
-        findViewById(R.id.ring_hang_up_button).setOnClickListener(view -> {
+        stopRingButton = findViewById(R.id.ring_hang_up_button);
+        stopRingButton.setOnClickListener(view -> {
             bindService(
                     new Intent(AlarmActivity.this, RingService.class),
                     connectionToRingService,
                     BIND_AUTO_CREATE
             );
         });
+        tryToAnimate();
+
+    }
+
+    /**
+     * This method tries to animate the stop ring button. If the Drawable in the stop ring button
+     * is not an {@link androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat}
+     * then no animation is played.
+     */
+    private void tryToAnimate() {
+        Drawable ringDrawable = stopRingButton.getDrawable();
+
+        if (!(ringDrawable instanceof AnimatedVectorDrawable)) return;
+        // We check and cast to AnimatedVectorDrawable, which requires api >= 21, but we then use
+        // the Compat animationCallback because the non Compat requires api >= 24.
+        AnimatedVectorDrawable animatedRingDrawable =
+                (AnimatedVectorDrawable) ringDrawable;
+
+        // Due to the fact the animation is organized as a set, the looping must be set
+        // programmatically.
+        AnimatedVectorDrawableCompat.registerAnimationCallback(
+                animatedRingDrawable,
+                new Animatable2Compat.AnimationCallback() {
+                    @Override
+                    public void onAnimationEnd(Drawable drawable) {
+                        super.onAnimationEnd(drawable);
+                        if (drawable instanceof AnimatedVectorDrawable)
+                            ((AnimatedVectorDrawable) drawable).start();
+                    }
+                }
+        );
+        animatedRingDrawable.start();
     }
 }
